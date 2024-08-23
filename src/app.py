a752fe6d513dc7433f0522de5fd859312c435bb4
owati <mofiyinfoluwaowati@gmail.com>
2024-08-23 08:46:21 +0100

    Added cors

--- src/app.py
+++ src/app.py
@@ -1,10 +1,12 @@
 import os
 import redis
 from flask import Flask
+from flask_cors import CORS
 from flask_socketio import SocketIO
 from src.controller.vehicles import vehicle_controller_factory
 
 app = Flask(__name__)
+cors = CORS(app)
 socket_io = SocketIO(app)
 
 redis_client = redis.from_url(os.getenv('REDIS_URL'))
